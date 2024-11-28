dd9e19f8a022c4710689e9af5148d46ebcdb2886
SarikaDubale <sarika9518@gmail.com>
2024-11-28 15:34:40 +0530

    day1

---
+++ Day1/src/Basic/day1/day1.java
@@ -0,0 +1,11 @@
+package Basic.day1;
+
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello sarika");
+		System.out.println("welcome to TNS foundation");
+	}
+
+}
